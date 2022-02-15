c03722232f9be53abcf65a9969c06ad474341f18
105297008 <105297008@L815WD150052.edu.cbe.ab.ca>
2022-02-15 11:50:05 -0700

    Added BinaryNumbers assignment.

---
+++ Chapter2/src/BinaryNumbers.java
@@ -0,0 +1,27 @@
+/*
+
+Program: BinaryNumbers.java          Last Date of this Revision: February 15, 2022
+
+Purpose: Display number 1-20 along with their binary form
+
+Author: Nathaniel Edillon
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
+public class BinaryNumbers {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Anyeonghaseyo");
+
+	}
+
+}
+
+/* Screen Dump
+
+
+*/
