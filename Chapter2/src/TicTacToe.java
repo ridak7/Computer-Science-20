808609f3c4cb212a099745c3f7302d67b96237c6
105297008 <105297008@L815WD150052.edu.cbe.ab.ca>
2022-02-15 10:59:27 -0700

    First commit; getting these projects into my repository.

---
+++ Chapter2/src/TicTacToe.java
@@ -0,0 +1,36 @@
+/*
+
+Program: TicTacToe.java          Last Date of this Revision: MMMMM DD, YYYY
+
+Purpose: xx
+
+Author: Nathaniel Edillon
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
+public class TicTacToe 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Time to make meincraft game!!");
+		System.out.println("Read my newest book, Meinkampf!");
+		
+		int food, clothing, entertainment;
+		food = 69;
+		clothing = 1;
+		System.out.println(food + clothing);
+	}
+
+}
+
+
+/* Screen Dump
+
+Paste the output of your code here
+ 
+ */
