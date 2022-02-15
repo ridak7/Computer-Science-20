cee033c382f7e4092ff0be972e4e7611ba133dd2
NattyEddy <heyitzn8@gmail.com>
2022-02-14 22:16:09 -0700

    init

--- src/Chapter2/App.java
+++ src/Chapter2/App.java
@@ -1,6 +1,7 @@
 package Chapter2;
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)
+    {
         System.out.println("Hello, World!");
     }
 }
