a8d06e750fdee4c25ef69e19a829079223229712
105297008 <105297008@L815WD150052.edu.cbe.ab.ca>
2022-02-16 10:17:01 -0700

    Created MyInitials assignment.

---
+++ Chapter2/src/MyInitials.java
@@ -0,0 +1,28 @@
+/*
+
+Program: MyInitials.java          Last Date of this Revision: February 16, 2022
+
+Purpose: Display my initials (NPE)
+
+Author: Nathaniel Edillon
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
+public class MyInitials {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+
+}
+
+/* Screen Dump
+
+
+
+
+*/
