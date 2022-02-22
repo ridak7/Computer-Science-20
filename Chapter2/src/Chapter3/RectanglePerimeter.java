d60364049da4f0d214e1ab48d9028008a54b113a
105297008 <105297008@L815WD150052.edu.cbe.ab.ca>
2022-02-22 11:57:12 -0700

    RectanglePerimeter assignment created and finished.

---
+++ Chapter2/src/Chapter3/RectanglePerimeter.java
@@ -0,0 +1,30 @@
+/*
+
+Program: VariableInClassExample.java          Last Date of this Revision: February 22, 2022
+
+Purpose: Take three numbers and add them together (context: distances in a race into total)
+
+Author: Nathaniel Edillon
+School: CHHS
+Course: Computer Science 20
+ 
+
+*/
+
+package Chapter3;
+
+public class RectanglePerimeter 
+{
+	public static void main(String[] args) 
+	{
+		int l = 13, w = 4;
+		int perimeter = 2 * (l + w);
+		System.out.print(perimeter);
+	}
+}
+
+/* Screen dump 
+
+34
+
+*/
