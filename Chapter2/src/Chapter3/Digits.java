f1bab362089f4e19a1ee63121b2d1e65547d5df5
105297008 <105297008@L815WD150052.edu.cbe.ab.ca>
2022-02-24 10:55:27 -0700

    Finished Digits assignment.

--- Chapter2/src/Chapter3/Digits.java
+++ Chapter2/src/Chapter3/Digits.java
@@ -1,3 +1,15 @@
+/*
+
+Program: Digits.java          Last Date of this Revision: February 24, 2022
+
+Purpose: Prompt the user to input a two digit number, then display the digits in the tens and ones place.
+
+Author: Nathaniel Edillon
+School: CHHS
+Course: Computer Science 20
+
+*/
+
 package Chapter3;
 
 import java.util.Scanner;
@@ -7,6 +19,24 @@ public class Digits
 	public static void main(String[] args) 
 	{
 		Scanner userInput = new Scanner (System.in);
+		
+		System.out.print("Input a two digit number: ");
+		int userNumber = userInput.nextInt();
+		System.out.println();
+		
+		int tens = userNumber / 10, ones = userNumber % 10;
+		
+		System.out.println("Tens: " + tens);
+		System.out.println("Ones: " + ones);
+		
 	}
-
 }
+
+/* Screen dump 
+
+Input a two digit number: 69
+
+Tens: 6
+Ones: 9
+
+*/
