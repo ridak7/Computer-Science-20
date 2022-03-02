1ed3e8cb446ad92fe7f3aa583a2cc96e8886a91c
105297008 <105297008@L815WD150052.edu.cbe.ab.ca>
2022-03-02 10:18:30 -0700

    Finished MathTutorExercise.

--- Chapter2/src/Chapter4/MathTutorExercise.java
+++ Chapter2/src/Chapter4/MathTutorExercise.java
@@ -1,3 +1,16 @@
+/*
+
+Program: MathTutorExercise.java          Last Date of this Revision: March 2, 2022
+
+Purpose: Prompt the user to answer the generated equation and compare to the solution.
+
+Author: Nathaniel Edillon
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
 package Chapter4;
 
 import java.util.Scanner;
@@ -7,8 +20,11 @@ public class MathTutorExercise
 	public static void main(String[] args)
 	{
 		Scanner userInput = new Scanner(System.in);
-		int value1 = (int) ((10) * Math.random() + 1), value2 = (int) ((10) * Math.random() + 1), operationRNG = (int) ((4) * Math.random() + 1), maths;
-		char operation;
+		int value1 = (int) ((10) * Math.random() + 1), 
+			value2 = (int) ((10) * Math.random() + 1), 
+			operationRNG = (int) ((4) * Math.random() + 1);
+		double userAnswer = 0, maths = 0, doubleValue1;
+		char operation = '0';
 		
 		switch (operationRNG)
 		{
@@ -22,13 +38,35 @@ public class MathTutorExercise
 				break;
 			case 3:
 				maths = value1 * value2;
-				operation = '*';
+				operation = 'x';
 				break;
 			case 4:
-				maths = value1 / value2;
+				doubleValue1 = value1;
+				maths = doubleValue1 / value2;
 				operation = '/';
 				break;
 		}
+		
+		System.out.printf("%d %c %d = ", value1, operation, value2);
+		userAnswer = userInput.nextDouble();
+		
+		if (userAnswer == maths)
+		{
+			System.out.println("Correct!");
+		}
+		else
+		{
+			System.out.printf("Incorrect. The correct answer is %.2f", maths);
+		}
 	}
-
 }
+
+/* Screen Dump
+ 
+5 + 1 = 4
+Incorrect. The correct answer is 6.00 
+ 
+5 x 2 = 10
+Correct!
+ 
+*/
