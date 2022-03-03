fdeb7951dba4c8e822ddd676a0678bbb5810fb23
105297008 <105297008@L815WD150052.edu.cbe.ab.ca>
2022-03-03 11:29:10 -0700

    Completed OddSum assignment.

--- Chapter2/src/Chapter5/OddSum.java
+++ Chapter2/src/Chapter5/OddSum.java
@@ -1,10 +1,46 @@
+/*
+
+Program: OddSum.java          Last Date of this Revision: March 3, 2022
+
+Purpose: Prompt the user to input a number, then adds all odd numbers between 1 and their number and prints the result.
+
+Author: Nathaniel Edillon
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
 package Chapter5;
 
-public class OddSum {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class OddSum
+{
+	public static void main(String[] args)
+	{
+		Scanner userInput = new Scanner(System.in);
+		int sum = 0, userNumber;
+		
+		System.out.print("Enter a natural number: ");
+		userNumber = userInput.nextInt();
+		
+		// add x to the sum if it is odd
+		for (int x = 1; x <= userNumber; x++)
+		{
+			if (x % 2  == 1)
+			{
+				sum += x;
+			}
+		}
+		
+		System.out.printf("The sum is %d.", sum);
 	}
-
 }
+
+/*
+
+Enter a natural number: 9
+The sum is 25.
+
+*/
