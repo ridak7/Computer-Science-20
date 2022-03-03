bdd2f9da62e6f4a612758973ebc4d43cb651548f
105297008 <105297008@L815WD150052.edu.cbe.ab.ca>
2022-03-03 11:52:30 -0700

    Added comments to AccountSetup (header, screen dump etc.)

--- Chapter2/src/Chapter5/AccountSetup.java
+++ Chapter2/src/Chapter5/AccountSetup.java
@@ -1,3 +1,16 @@
+/*
+
+Program: AccountSetup.java          Last Date of this Revision: March 3, 2022
+
+Purpose: Prompt the user to input a username and password, which must require 8 characters, and print them in lowercase.
+
+Author: Nathaniel Edillon
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
 package Chapter5;
 
 import java.util.Scanner;
@@ -12,6 +25,7 @@ public class AccountSetup
 		System.out.print("Enter a username: ");
 		username = userInput.nextLine();
 		
+		// ask for a password, then repeat if the password is lesser than 8 characters long
 		do
 		{
 			System.out.print("Enter a password that is at least 8 characters: ");
@@ -22,3 +36,12 @@ public class AccountSetup
 		System.out.printf("Your username is %s and your password is %s", username.toLowerCase(), password.toLowerCase());
 	}
 }
+
+/*
+
+Enter a username: NATTYEDDY
+Enter a password that is at least 8 characters: TriCk
+Enter a password that is at least 8 characters: n00bMAst3r69
+Your username is nattyeddy and your password is n00bmast3r69
+
+*/
