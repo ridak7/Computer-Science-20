f504577f9c4526a4045fc4ee68f460601b7fbff2
NattyEddy <heyitzn8@gmail.com>
2022-03-03 18:55:39 -0700

    Added to Evens assignment (now complete).

--- Chapter2/src/Chapter5/Evens.java
+++ Chapter2/src/Chapter5/Evens.java
@@ -2,7 +2,7 @@
 
 Program: Evens.java          Last Date of this Revision: March 3, 2022
 
-Purpose: Print all the even numbers between 1-20.
+Purpose: Print all the even numbers between 1-20 and its sum.
 
 Author: Nathaniel Edillon
 School: CHHS
@@ -17,13 +17,16 @@ public class Evens
 {
 	public static void main(String[] args)
 	{
+		int sum = 0;
 		for (int x = 1; x <= 20; x++)
 		{
 			if (x % 2 == 0)
 			{
+				sum += x;
 				System.out.println(x);
 			}
 		}
+		System.out.printf("The sum of all these numbers is %d.", sum);
 	}
 }
 
@@ -39,6 +42,6 @@ public class Evens
 16
 18
 20
-
+The sum of all these numbers is 110.
 
 */
